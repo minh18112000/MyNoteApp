7a913ad0527594fa1bbdd45c77621067f8fc14e0
minh18112000 <nguyenducminh_t63@hus.edu.vn>
2021-09-22 17:16:17 +0700

    Design layout for App

--- app/src/main/java/com/example/mynoteapp/MainActivity.kt
+++ app/src/main/java/com/example/mynoteapp/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.mynoteapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mynoteapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(binding.toolbar)
     }
 }
