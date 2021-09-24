8a79be873a25107b2a268f3d1b59ae93689461a6
minh18112000 <nguyenducminh_t63@hus.edu.vn>
2021-09-24 21:55:25 +0700

    Update successfully

--- app/src/main/java/com/example/mynoteapp/fragments/UpdateNoteFragment.kt
+++ app/src/main/java/com/example/mynoteapp/fragments/UpdateNoteFragment.kt
@@ -3,10 +3,12 @@ package com.example.mynoteapp.fragments
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.example.mynoteapp.MainActivity
 import com.example.mynoteapp.R
 import com.example.mynoteapp.databinding.FragmentUpdateNoteBinding
 import com.example.mynoteapp.model.Note
+import com.example.mynoteapp.toast
 import com.example.mynoteapp.viewmodel.NoteViewModel
 
 class UpdateNoteFragment : Fragment(R.layout.fragment_update_note) {
@@ -17,11 +19,6 @@ class UpdateNoteFragment : Fragment(R.layout.fragment_update_note) {
 
     private lateinit var currentNote: Note
     private lateinit var noteViewModel: NoteViewModel
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setHasOptionsMenu(true)
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -47,12 +44,33 @@ class UpdateNoteFragment : Fragment(R.layout.fragment_update_note) {
         binding.etNoteTitleUpdate.setText(currentNote.noteTitle)
         binding.etNoteBodyUpdate.setText(currentNote.noteBody)
 
+        binding.fabDone.setOnClickListener {
+            val title = binding.etNoteTitleUpdate.text.toString().trim()
+            val body = binding.etNoteBodyUpdate.text.toString().trim()
+
+            if (title.isNotEmpty()) {
+                val note = Note(currentNote.id, title, body)
+                noteViewModel.updateNote(note)
+
+                activity?.toast("Note updated!")
+
+                // Using directions to navigate to the HomeFragment
+                view?.findNavController()?.navigate(
+                    UpdateNoteFragmentDirections.actionUpdateNoteFragmentToHomeFragment()
+                )
+            } else {
+                activity?.toast("Please enter title!")
+            }
+        }
+
+
+        setHasOptionsMenu(true)
         return binding.root
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         super.onCreateOptionsMenu(menu, inflater)
-        inflater.inflate(R.menu.update_menu,menu)
+        inflater.inflate(R.menu.update_menu, menu)
     }
 
     override fun onDestroy() {
