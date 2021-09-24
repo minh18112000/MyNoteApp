519c08ceaaeafee35e291e5526075ab34bd6351f
minh18112000 <nguyenducminh_t63@hus.edu.vn>
2021-09-24 11:17:33 +0700

    Setup Repository

---
+++ app/src/main/java/com/example/mynoteapp/repository/NoteRepository.kt
@@ -0,0 +1,11 @@
+package com.example.mynoteapp.repository
+
+import com.example.mynoteapp.database.NoteDatabase
+import com.example.mynoteapp.model.Note
+
+class NoteRepository(private val db: NoteDatabase) {
+    suspend fun addNote(note: Note) = db.getNoteDao().addNote(note)
+    suspend fun updateNote(note: Note) = db.getNoteDao().updateNote(note)
+    suspend fun deleteNote(note: Note) = db.getNoteDao().deleteNote(note)
+    fun getAllNotes() = db.getNoteDao().getAllNotes()
+}
